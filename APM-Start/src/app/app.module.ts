950368d81ea75b382d3918c4090a4b4efa4ca0ac
kasia.mossakowska <kasia.mossakowska@macrix.pl>
2021-02-04 16:42:44 +0100

    Add service for getting products, root injector

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
 import { FormsModule } from '@angular/forms';
-import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
+import { ConvertToSpacesPipe } from './shared/pipes/convert-to-spaces.pipe';
 import { StarComponent } from './shared/star-component/star.component';
 
 @NgModule({
--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { IProduct } from "./product";
+import { ProductService } from "../shared/services/product.service";
 
 @Component({
     selector: 'pm-products',
@@ -11,7 +12,6 @@ export class ProductListComponent implements OnInit {
     pageTitle: string = 'Product List';
     imageWidth: number = 50;
     imageMargin: number = 2;
-
     showImage: boolean = false;
 
     _listFilter: string;
@@ -26,46 +26,15 @@ export class ProductListComponent implements OnInit {
 
     filteredProducs: IProduct[];
 
-    products: IProduct[] = [
-        {
-          "productId": 1,
-          "productName": "Leaf Rake",
-          "productCode": "GDN-0011",
-          "releaseDate": "March 19, 2019",
-          "description": "Leaf rake with 48-inch wooden handle.",
-          "price": 19.95,
-          "starRating": 3.2,
-          "imageUrl": "assets/images/leaf_rake.png"
-        },
-        {
-          "productId": 2,
-          "productName": "Garden Cart",
-          "productCode": "GDN-0023",
-          "releaseDate": "March 18, 2019",
-          "description": "15 gallon capacity rolling garden cart",
-          "price": 32.99,
-          "starRating": 4.2,
-          "imageUrl": "assets/images/garden_cart.png"
-        },
-        {
-          "productId": 5,
-          "productName": "Hammer",
-          "productCode": "TBX-0048",
-          "releaseDate": "May 21, 2019",
-          "description": "Curved claw steel hammer",
-          "price": 8.9,
-          "starRating": 4.8,
-          "imageUrl": "assets/images/hammer.png"
-        }
-    ];
+    products: IProduct[];
 
-    constructor() {
-      this.filteredProducs = this.products;
-      this.listFilter = 'cart';
+    constructor(private productService: ProductService) {
+      //constructor is executed before ngOnInit()
     }
 
     ngOnInit(): void {
-      console.log('In OnInit');
+      this.products = this.productService.getProducts();
+      this.filteredProducs = this.products;
     }
 
     performFilter(filteredBy: string): IProduct[] {
